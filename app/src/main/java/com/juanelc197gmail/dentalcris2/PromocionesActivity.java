30aa62e4437bfa21af0235a381f0d4ab48f73779
Juanelc197 <juanelc197@outlook.com>
2017-03-25 18:05:06 -0600

    Agregando webview con la respectiva página de Facebook para las promociones.

--- app/src/main/java/com/juanelc197gmail/dentalcris2/PromocionesActivity.java
+++ app/src/main/java/com/juanelc197gmail/dentalcris2/PromocionesActivity.java
@@ -8,12 +8,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.ImageButton;
 
 public class PromocionesActivity extends AppCompatActivity {
 
+    WebView wv;
+
     //private ImageButton imbtn;
+
+    public void onBackPressed() {
+        if (wv.canGoBack()) {
+            wv.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +40,25 @@ public class PromocionesActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
+
+        wv = (WebView) findViewById(R.id.webv);
+
+        //Habilitar JavaScrip
+        wv.getSettings() .setJavaScriptEnabled(true);
+        wv.setFocusable(true);
+        wv.setFocusableInTouchMode(true);
+
+        // Establezca la prioridad de render a alta
+        wv.getSettings() .setRenderPriority(WebSettings.RenderPriority.HIGH);
+        wv.getSettings() .setCacheMode(WebSettings.LOAD_NO_CACHE);
+        wv.getSettings() .setDomStorageEnabled(true);
+        wv.getSettings() .setDatabaseEnabled(true);
+        wv.getSettings() .setAppCacheEnabled(true);
+        wv.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+
+        //Cargar link
+        wv.loadUrl("https://www.facebook.com/criisty.loopezz.3?ref=ts&fref=ts");
+        wv.setWebViewClient(new WebViewClient());
 
         //imbtn = (ImageButton) findViewById(R.id.img_btn);
 
